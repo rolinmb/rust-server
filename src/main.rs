d619dc26140d35114f891eaf12fb13cd839e9352
Rolin Blake <rolinmblake@gmail.com>
2023-08-23 13:07:13 -0500

    couldnt add an html file to the served content

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn handle_read(mut stream: &TcpStream) {
 }
 
 fn handle_write(mut stream: TcpStream) {
-  let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Rust does stuff</body></html>\r\n";
+  let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Rust is kindof cool</body></html>\r\n";
   match stream.write(response) {
     Ok(_) => println!("Response sent"),
     Err(e) => println!("Failed sending response: {}", e),
@@ -34,9 +34,7 @@ fn main() {
   for stream in listener.incoming() {
     match stream {
       Ok(stream) => {
-        thread::spawn(|| {
-          handle_client(stream)
-        });
+        thread::spawn(|| {handle_client(stream)});
       }
       Err(e) => {
         println!("Unable to connect: {}", e);
